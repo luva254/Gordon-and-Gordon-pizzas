0e18d2912153f102e58bafd391947451938ff6cb
luva/254 <godfrey.luvandale@student.moringaschool.com>
2021-12-01 14:25:59 +0300

    add idex.html styles.css script.js

---
+++ js/script.js
@@ -0,0 +1,58 @@
+function order(){
+
+var delivery=document.getElementById("delivery").value
+
+
+        if(delivery=='yes'){
+            var location=prompt('Enter Your Location')
+            alert('Your pizza will be delivered at ' + location)
+        }
+        else{
+            alert('Come pick your pizza at Gordons&Gordons store')
+        }
+
+
+}
+function checkout(){
+    var pizzaSize=document.getElementById("pizza-sizes").value
+var pizzaCrust=document.getElementById("pizza-crusts").value
+var pizzaTopping=document.getElementById("pizza-toppings").value
+var numberOfPizzas=parseInt(document.getElementById("number").value)
+var delivery=document.getElementById("delivery").value
+
+if(pizzaSize=='small'){
+    price1=300
+}
+else if(pizzaSize=='medium'){
+    price1=600
+}
+else(price=900)
+
+if(pizzaCrust=='Stuffed'){
+    price2=100
+}
+else if(pizzaCrust=='thin'){
+    price2=150
+}
+else if(pizzaCrust=='thick'){
+    price2=250
+}
+else(price=180)
+if(pizzaTopping=='brocolli'){
+    price3=150
+}
+else if(pizzaTopping=='sausage and pancetta'){
+    price3=300
+}
+else if(pizzaTopping=='mushroom'){
+    price3=100
+}
+else(price=260)
+var pizzaPrices=(price1+price2+price3)*numberOfPizzas
+alert('Your total price is' + pizzaPrices)
+}
+
+
+
+
+
